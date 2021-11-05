0b281a3a4237d43e538153a024103faa0f27a887
Ruslan K <rus-blood@yandex.ru>
2021-11-05 20:17:20 +0300

    Empty project, generated by start.spring.io.

---
+++ src/test/java/ru/guru/part2/webapp/WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.guru.part2.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
